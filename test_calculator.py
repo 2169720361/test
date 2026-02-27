75c58c245372783beb666615959a6ff5c6c3102b
user <yx20251215@outlook.com>
2026-02-27 23:39:45 +0800

    修正

--- test_calculator.py
+++ test_calculator.py
@@ -4,7 +4,7 @@ from calculator import add,multiply
 
 def test_add():
     assert add(2,3) == 5
-    assert multiply(-1,1) == 0
+    assert add(-1,1) == 0
 def test_multiply():
     assert multiply(3,4) == 12
     assert multiply(2,0) == 0
